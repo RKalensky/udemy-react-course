80b06c5f04fa67149c8f96256c4554594b792275
roma.kalensky <roma.kalensky@gmail.com>
2020-06-07 21:35:16 +0300

    add(containers): new root components

---
+++ src/containers/QuizList/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class QuizList extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Quiz List</h1>
+            </div>
+        )
+    }
+}
