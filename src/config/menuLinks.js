3f8ebfc73e8015d18ab87f07195e338229995fa8
roma.kalensky <roma.kalensky@gmail.com>
2020-06-07 21:46:13 +0300

    add(config): menu links

---
+++ src/config/menuLinks.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        id: 'quizList',
+        to: '/',
+        label: 'Quiz List',
+        exact: true
+    },
+    {
+        id: 'auth',
+        to: '/auth',
+        label: 'Sign in',
+        exact: false
+    },
+    {
+        id: 'createQuiz',
+        to: '/Quiz Creator',
+        label: 'Create Quiz',
+        exact: false
+    }
+];
