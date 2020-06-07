553b572e5de9e53181ee23706e2921301be6c721
roma.kalensky <roma.kalensky@gmail.com>
2020-06-07 23:24:38 +0300

    update(component): QuizList, logic, styles

--- src/containers/QuizList/index.js
+++ src/containers/QuizList/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './index.styl';
 
 export default class QuizList extends React.Component {
+    renderQuizList = () => {
+        return [1, 2, 3].map((quiz, index) => {
+            return (
+                <NavLink key={index} to={`/quiz/${quiz}`}>
+                    <li>
+                        Test { quiz }
+                    </li>
+                </NavLink>
+            );
+        })
+    }
+
     render() {
         return (
-            <div>
+            <div className='quiz-list'>
                 <h1>Quiz List</h1>
+                <ul>
+                    { this.renderQuizList() }
+                </ul>
             </div>
         )
     }
