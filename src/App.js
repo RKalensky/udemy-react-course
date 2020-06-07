87e3b16870ec612636c3188ecf0a329c63536ff2
roma.kalensky <roma.kalensky@gmail.com>
2020-06-07 21:38:57 +0300

    update(App): add routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Layout from './hoc/Layout/index';
+import { Route, Switch } from 'react-router-dom';
+import Auth from './containers/Auth';
 import Quiz from './containers/Quiz';
+import QuizCreator from './containers/QuizCreator';
+import QuizList from './containers/QuizList';
 
 function App() {
   return (
      <Layout>
-       <Quiz></Quiz>
+       <Switch>
+         <Route path='/auth' component={ Auth }/>
+         <Route path='/quiz-creator' component={ QuizCreator }/>
+         <Route path='/quiz/:id' component={ Quiz }/>
+         <Route path='/' component={ QuizList }/>
+       </Switch>
      </Layout>
   ); 
 }
